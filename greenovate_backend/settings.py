13b80edd49c1906f9e5e2aeb12c80410274cd145
Satwik Pandey <shashwatpandey@Satwiks-MacBook-Air.local>
2023-09-25 11:34:08 +0530

    test

--- greenovate_backend/settings.py
+++ greenovate_backend/settings.py
@@ -56,9 +56,9 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
 
 ROOT_URLCONF = 'greenovate_backend.urls'
 
